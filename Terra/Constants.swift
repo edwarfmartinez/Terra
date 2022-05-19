f27a2bb7ddaa3ea16c717ad55f7dec1e1a8433d5
EdwarMartinez <edwarmartinez@yahoo.com>
2022-05-18 19:19:16 -0500

    Include Generics for Api calls and detailed view for satellite image

---
+++ Terra/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  Terra
+//
+//  Created by EDWAR FERNANDO MARTINEZ CASTRO on 16/05/22.
+//
+
+import Foundation
---
+++ Terra/Model/FieldDetailData.swift
@@ -0,0 +1,8 @@
+//
+//  FieldDetailData.swift
+//  Terra
+//
+//  Created by EDWAR FERNANDO MARTINEZ CASTRO on 17/05/22.
+//
+
+import Foundation
